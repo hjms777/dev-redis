b7deb5191e9abff43dededa867d36ed31763494a
hjms777 <hjms777@naver.com>
2025-11-04 14:44:24 +0900

    Initial commit

---
+++ src/main/java/com/example/dev_redis/DevRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.dev_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dev_redis/DevRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dev_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
